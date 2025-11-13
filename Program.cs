5e741e9281b6acdccbf027f7087c6d698da8e7e9
James McKinney <jamesmckinney19@gmail.com>
2025-11-13 01:07:56 -0500

    Slapped in some code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿Random random = new Random();
+ 
+string[] pokemon = ["Bulbasaur", "Squirtle", "Charmander", "EMPTY", "EMPTY", "EMPTY"];
+string[] encounters = ["Natu", "Sandshrew", "Ledyba", "Sentret", "Geodude", "Magikarp"];
+
+int index = random.Next(encounters.Length);
+
+Console.WriteLine("\nStarting Team\n-------------");
+
+for (int i = 0; i < pokemon.Length; i++)
+{
+    Console.WriteLine(pokemon[i]);
+}
+
+Console.WriteLine("\nWild Pokemon!\n-------------");
+
+for (int i = 0; i < encounters.Length; i++)
+{
+    Console.WriteLine(encounters[i]);
+}
+
+if (pokemon[3] == "EMPTY")
+{
+    pokemon[3] = encounters[index];
+}
+
+if (pokemon[4] == "EMPTY")
+{
+    index = random.Next(encounters.Length);
+    pokemon[4] = encounters[index];
+}
+
+if (pokemon[5] == "EMPTY")
+{
+    index = random.Next(encounters.Length);
+    pokemon[5] = encounters[index];
+}
+
+Console.WriteLine("\nNew Party!\n----------");
+
+for (int i = 0; i < pokemon.Length; i++)
+{
+    Console.WriteLine(pokemon[i]);
+}
--- obj/Debug/net9.0/week-four-deliverable.AssemblyInfo.cs
+++ obj/Debug/net9.0/week-four-deliverable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week-four-deliverable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c2b4c86bb5273493e32b1d885c447cdfe6ea49")]
 [assembly: System.Reflection.AssemblyProductAttribute("week-four-deliverable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week-four-deliverable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
